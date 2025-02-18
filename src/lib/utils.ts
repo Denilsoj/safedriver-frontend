0811975795fbde4a2c396259c52b4d2696be1400
Denilson Oliveira <juniod04@gmail.com>
2025-02-18 04:19:33 -0300

    feat: add function for mask number phone

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,3 +39,11 @@ export const maskCPF = (value: string): string => {
 		.replace(/(\d{3})(\d)/, "$1.$2")
 		.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
 };
+
+export const maskPhone = (value: string): string => {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{2})(\d)/, "($1) $2")
+		.replace(/(\d{5})(\d)/, "$1-$2")
+		.slice(0, 15);
+};
