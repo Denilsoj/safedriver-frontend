7a7210d6b23c713d73939112072ce915dac38a38
Denilson Oliveira <juniod04@gmail.com>
2025-02-21 11:24:10 -0300

    fix: refactor maskPhone

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -41,9 +41,12 @@ export const maskCPF = (value: string): string => {
 };
 
 export const maskPhone = (value: string): string => {
-	return value
-		.replace(/\D/g, "")
-		.replace(/(\d{2})(\d)/, "($1) $2")
-		.replace(/(\d{5})(\d)/, "$1-$2")
-		.slice(0, 15);
+	if (value) {
+		return value
+			.replace(/\D/g, "")
+			.replace(/(\d{2})(\d)/, "($1) $2")
+			.replace(/(\d{5})(\d)/, "$1-$2")
+			.slice(0, 15);
+	}
+	return "";
 };
