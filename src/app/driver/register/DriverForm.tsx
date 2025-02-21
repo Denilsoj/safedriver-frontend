71e605743475302fbe7d207af832059652a9a2a2
Denilson Oliveira <juniod04@gmail.com>
2025-02-21 11:19:03 -0300

    style: add responsiveness

--- src/app/driver/register/DriverForm.tsx
+++ src/app/driver/register/DriverForm.tsx
@@ -9,20 +9,22 @@ export function DriverForm() {
 	const { step } = useDriverForm();
 
 	return (
-		<div className="max-w-2xl mx-auto p-6 h-full flex flex-col justify-center">
-			<header className="flex justify-center items-center">
-				<img
-					src="../../../../logo.png"
-					alt="logo"
-					className="max-w-36 min-w-16 "
-				/>
-			</header>
-			{step === 0 && <PersonalInfoStep />}
-			{step === 1 && <AddressStep />}
-			{step === 2 && <DocumentsStep />}
+		<div className="w-full p-6 h-full flex flex-col justify-center">
+			<div className="mx-auto bg-white p-5 rounded-lg w- shadow-2xl sm:w-2/5">
+				<header className="flex justify-center items-center">
+					<img
+						src="../../../../logo.png"
+						alt="logo"
+						className="max-w-36 min-w-16 "
+					/>
+				</header>
+				{step === 0 && <PersonalInfoStep />}
+				{step === 1 && <AddressStep />}
+				{step === 2 && <DocumentsStep />}
 
-			<div className="mt-4">
-				<StepIndicator currentStep={step} totalSteps={3} />
+				<div className="mt-4">
+					<StepIndicator currentStep={step} totalSteps={3} />
+				</div>
 			</div>
 		</div>
 	);
