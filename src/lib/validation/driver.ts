4800051e84f0e34d4ebe2df8e5cf3df1f19e1b52
Denilson Oliveira <juniod04@gmail.com>
2025-02-24 14:10:01 -0300

    fix: refactor logic of files validated

--- src/lib/validation/driver.ts
+++ src/lib/validation/driver.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
 import { isAdult } from "../utils";
-const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const MAX_FILE_SIZE = 10 * 1024 * 1024;
 const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
+const isBrowser = typeof window !== "undefined";
 export const driverSchema = z.object({
 	personalInfo: z.object({
 		name: z.string().min(3, "Nome deve ter no mínimo 3 caracteres"),
 		email: z.string().email("Email inválido"),
 		cpf: z.string().regex(/^\d{11}$/, "CPF inválido"),
-		phone: z.string().regex(/^\d{11}$/, "Telefone inválido"),
+		telephone: z.union([
+			z
+				.string()
+				.regex(/^\d{11}$/, "Telefone inválido")
+				.optional(),
+			z.any().optional(),
+		]),
 		date_birth: z
 			.date({
 				required_error: "Data de nascimento é obrigatória",
@@ -20,7 +27,7 @@ export const driverSchema = z.object({
 			.refine((date) => date >= new Date("1900-01-01"), {
 				message: "Data deve estar entre 01/01/1900 e hoje",
 			})
-			.refine((dateStr) => isAdult, {
+			.refine((dateStr) => isAdult(dateStr), {
 				message: "Motorista deve ter pelo menos 18 anos",
 			})
 			.transform((date) => date.toISOString().split("T")[0])
@@ -36,48 +43,48 @@ export const driverSchema = z.object({
 	}),
 	documents: z.object({
 		src_cnh: z
-			.instanceof(FileList)
-			.refine((files) => files.length > 0, {
+			.instanceof(File)
+			.refine((files) => files.size > 0, {
 				message: "CNH é obrigatória",
 			})
 			.refine(
 				(files) => {
-					const file = files[0];
-					const validTypes = ["image/jpeg", "image/png", "application/pdf"];
+					const file = files;
+					const validTypes = ["image/jpeg", "image/png", "image/jpg"];
 					return validTypes.includes(file.type);
 				},
 				{
-					message: "Arquivo deve ser JPG, PNG ou PDF",
+					message: "Arquivo deve ser JPG, PNG ou JPEG",
 				},
 			)
 			.refine(
 				(files) => {
-					const file = files[0];
-					return file.size <= 5 * 1024 * 1024; // 5MB
+					const file = files;
+					return file.size <= 10 * 1024 * 1024;
 				},
 				{
 					message: "O arquivo deve ter no máximo 5MB",
 				},
 			),
 		src_crlv: z
-			.instanceof(FileList)
-			.refine((files) => files.length > 0, {
+			.instanceof(File)
+			.refine((files) => files.size > 0, {
 				message: "CRLV é obrigatório",
 			})
 			.refine(
 				(files) => {
-					const file = files[0];
-					const validTypes = ["image/jpeg", "image/png", "application/pdf"];
+					const file = files;
+					const validTypes = ["image/jpeg", "image/png", "image/jpg"];
 					return validTypes.includes(file.type);
 				},
 				{
-					message: "Arquivo deve ser JPG, PNG ou PDF",
+					message: "Arquivo deve ser JPG, PNG ou JPEG",
 				},
 			)
 			.refine(
 				(files) => {
-					const file = files[0];
-					return file.size <= 5 * 1024 * 1024; // 5MB
+					const file = files;
+					return file.size <= 10 * 1024 * 1024; // 5MB
 				},
 				{
 					message: "O arquivo deve ter no máximo 5MB",
@@ -92,22 +99,22 @@ export const driverFormSchemaUpdated = z.object({
 	cpf: z.string().regex(/^\d{11}$/, "CPF inválido"),
 	telephone: z.string().regex(/^\d{11}$/, "Telefone inválido"),
 	date_birth: z
-		.string({
+		.any({
 			required_error: "Data de nascimento é obrigatória",
 			invalid_type_error: "Data de nascimento inválida",
 		})
-		.refine(
-			(dateStr) => {
-				const date = new Date(dateStr);
-				return date <= new Date() && date >= new Date("1900-01-01");
-			},
-			{
-				message: "Data deve estar entre 01/01/1900 e hoje",
-			},
-		)
-		.refine(isAdult, {
+		.refine((date) => new Date(date))
+		.refine((date) => date <= new Date(), {
+			message: "Data deve ser no máximo a data de hoje",
+		})
+		.refine((date) => date >= new Date("1900-01-01"), {
+			message: "Data deve estar entre 01/01/1900 e hoje",
+		})
+		.refine((dateStr) => isAdult(dateStr), {
 			message: "Motorista deve ter pelo menos 18 anos",
-		}),
+		})
+		.transform((date) => date.toISOString().split("T")[0])
+		.optional(),
 	status: z.enum(["Ativo", "Inativo"]),
 	address: z.object({
 		street: z.string().min(1, "Rua é obrigatória"),
@@ -116,81 +123,87 @@ export const driverFormSchemaUpdated = z.object({
 		state: z.string().min(1, "Estado é obrigatório"),
 		zip_code: z.string().min(1, "CEP é obrigatório"),
 	}),
-	src_cnh: z
-		.any()
-		.refine(
-			(file) => !file || (file.length > 0 && file[0].size <= MAX_FILE_SIZE),
-			"Arquivo deve ter no máximo 5MB",
-		)
-		.refine(
-			(file) =>
-				!file ||
-				(file.length > 0 && ACCEPTED_FILE_TYPES.includes(file[0].type)),
-			"Formato de arquivo inválido. Por favor, selecione uma imagem nos formatos PNG, JPEG, JPG.",
-		)
-		.optional(),
-	src_crlv: z
-		.any()
-		.refine(
-			(file) => !file || (file.length > 0 && file[0].size <= MAX_FILE_SIZE),
-			"Arquivo deve ter no máximo 5MB",
-		)
-		.refine(
-			(file) =>
-				!file ||
-				(file.length > 0 && ACCEPTED_FILE_TYPES.includes(file[0].type)),
-			"Formato de arquivo inválido. Por favor, selecione uma imagem nos formatos PNG, JPEG, JPG.",
-		)
-		.optional(),
+	src_cnh: z.union([
+		z
+			.any()
+			.refine(
+				(files) => !files || files[0].size <= MAX_FILE_SIZE,
+				"Arquivo deve ter no máximo 10MB!",
+			)
+			.refine(
+				(file) =>
+					!file ||
+					(file.length > 0 && ACCEPTED_FILE_TYPES.includes(file[0].type)),
+				"Formato de arquivo inválido. Por favor, selecione uma imagem nos formatos PNG, JPEG, JPG.",
+			)
+			.optional(),
+		z.string().optional(),
+	]),
+	src_crlv: z.union([
+		z
+			.any()
+			.refine(
+				(files) => !files || files[0].size <= MAX_FILE_SIZE,
+				"Arquivo deve ter no máximo 10MB!",
+			)
+			.refine(
+				(file) =>
+					!file ||
+					(file.length > 0 && ACCEPTED_FILE_TYPES.includes(file[0].type)),
+				"Formato de arquivo inválido. Por favor, selecione uma imagem nos formatos PNG, JPEG, JPG.",
+			)
+			.optional(),
+		z.string().optional(),
+	]),
 });
 
 export const enhancedDocumentSchema = z.object({
 	src_cnh: z
-		.instanceof(FileList)
-		.refine((files) => files.length > 0, {
+		.instanceof(File)
+		.refine((files) => files.size > 0, {
 			message: "CNH é obrigatória",
 		})
 		.refine(
 			(files) => {
-				const file = files[0];
-				const validTypes = ["image/jpeg", "image/png", "application/pdf"];
+				const file = files;
+				const validTypes = ["image/jpeg", "image/png", "image/jpg"];
 				return validTypes.includes(file.type);
 			},
 			{
-				message: "Arquivo deve ser JPG, PNG ou PDF",
+				message: "Arquivo deve ser JPG, PNG ou JPEG",
 			},
 		)
 		.refine(
 			(files) => {
-				const file = files[0];
-				return file.size <= 5 * 1024 * 1024; // 5MB
+				const file = files;
+				return file.size <= MAX_FILE_SIZE;
 			},
 			{
-				message: "O arquivo deve ter no máximo 5MB",
+				message: "O arquivo deve ter no máximo 10MB",
 			},
 		),
 	src_crlv: z
-		.instanceof(FileList)
-		.refine((files) => files.length > 0, {
+		.instanceof(File)
+		.refine((files) => files.size > 0, {
 			message: "CRLV é obrigatório",
 		})
 		.refine(
 			(files) => {
-				const file = files[0];
-				const validTypes = ["image/jpeg", "image/png", "application/pdf"];
+				const file = files;
+				const validTypes = ["image/jpeg", "image/png", "image/jpg"];
 				return validTypes.includes(file.type);
 			},
 			{
-				message: "Arquivo deve ser JPG, PNG ou PDF",
+				message: "Arquivo deve ser JPG, PNG ou JPEG",
 			},
 		)
 		.refine(
 			(files) => {
-				const file = files[0];
-				return file.size <= 5 * 1024 * 1024; // 5MB
+				const file = files;
+				return file.size <= MAX_FILE_SIZE;
 			},
 			{
-				message: "O arquivo deve ter no máximo 5MB",
+				message: "O arquivo deve ter no máximo 10MB",
 			},
 		),
 });
